54bb12f10a568ecaa0e604271014d09e115ea676
Amir Ahmadian <amir.ahmadian@outlook.com>
2021-09-10 11:28:33 +0200

    tweaks

--- src/examples/SocialNetwork/EncoverTests.java
+++ src/examples/SocialNetwork/EncoverTests.java
@@ -1,21 +1,21 @@
 /*
  * Copyright (C) 2021 Amir M. Ahmadian
  * 
- * This file is part of ENCoVer. ENCoVer is a JavaPathFinder extension allowing
+ * This file is part of DynCoVer. DynCoVer is a JavaPathFinder extension allowing
  * to verify if a Java method respects different epistemic noninterference
  * properties.
  * 
- * ENCoVer is free software: you can redistribute it and/or modify it under the
+ * DynCoVer is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
  * 
- * ENCoVer is distributed in the hope that it will be useful, but WITHOUT ANY
+ * DynCoVer is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License along with
- * ENCoVer. If not, see <http://www.gnu.org/licenses/>.
+ * DynCoVer. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
--- src/examples/SocialNetwork/User.java
+++ src/examples/SocialNetwork/User.java
@@ -1,23 +1,3 @@
-/*
- * Copyright (C) 2021 Amir M. Ahmadian
- * 
- * This file is part of TaxRecord. TaxRecord is the case study for task 4.1 of
- * the HATS project. It simulates a simplified tax paying process.
- * 
- * TaxRecord is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * TaxRecord is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with TaxRecord. If not, see <http://www.gnu.org/licenses/>.
- */
-
 /**
  * This class implements a user and its possible actions.
  * 
